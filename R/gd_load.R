d7ceb885ede2bb3507fbd83d39acdd4d3ea8efcf
Adrien Albaladejo <adrien.albaladejo@gmail.com>
2018-07-10 12:43:17 +0200

    Creating file and writing the import part

---
+++ R/gd_load.R
@@ -0,0 +1,41 @@
+#' @author A-BN / A-AB
+#' @description gd_load() import a gd (genome diff) file in a data frame
+#' @param gd_file path to the gd file
+#' @return a data frame
+#' @export
+
+library(dplyr)
+
+gd_load <-
+	function(gd_file) {
+		output_ <- read_delim(gd_file, 
+							 delim = "\t",
+							 comment='#',
+							 col_names=c('type',
+							 			'evidence-id',
+							 			'parent-ids',
+							 			'seq_id',
+							 			'position',
+							 			'a',  # temp column name for type-based calling
+							 			'b')) 
+		output <- 
+			output_ %>%
+				filter(type != 'RA', type != 'MC', type != 'JC', type != 'UN') %>%
+			
+				mutate(new_seq = 
+					   	case_when(
+					   		type == 'SNP' ~ a,
+					   		type == 'SUB' ~ b,
+					   		type == 'DEL' ~ '0'
+					   	)
+				) %>%
+				mutate(size = 
+								case_when(
+									type == 'SNP' ~ '0',
+									type == 'SUB' ~ a,
+									type == 'DEL' ~ a
+								)
+				) %>%
+				select(-a, -b)
+		
+	}
