7732f70f498efe14174d680562faec6dc3470777
Adrien Albaladejo <adrien.albaladejo@gmail.com>
2018-07-10 12:45:39 +0200

    Indenting correction

--- R/gd_load.R
+++ R/gd_load.R
@@ -1,4 +1,5 @@
 #' @author A-BN / A-AB
+#' @date 2018/07/10
 #' @description gd_load() import a gd (genome diff) file in a data frame
 #' @param gd_file path to the gd file
 #' @return a data frame
@@ -9,33 +10,33 @@ library(dplyr)
 gd_load <-
 	function(gd_file) {
 		output_ <- read_delim(gd_file, 
-							 delim = "\t",
-							 comment='#',
-							 col_names=c('type',
-							 			'evidence-id',
-							 			'parent-ids',
-							 			'seq_id',
-							 			'position',
-							 			'a',  # temp column name for type-based calling
-							 			'b')) 
+			delim = "\t",
+			comment='#',
+			col_names=c('type',
+				'evidence-id',
+				'parent-ids',
+				'seq_id',
+				'position',
+				'a',  # temp column name for type-based calling
+				'b')) 
+		
 		output <- 
 			output_ %>%
 				filter(type != 'RA', type != 'MC', type != 'JC', type != 'UN') %>%
 			
 				mutate(new_seq = 
-					   	case_when(
-					   		type == 'SNP' ~ a,
-					   		type == 'SUB' ~ b,
-					   		type == 'DEL' ~ '0'
-					   	)
+					case_when(
+					  	type == 'SNP' ~ a,
+					   	type == 'SUB' ~ b,
+					   	type == 'DEL' ~ '0'
+					 )
 				) %>%
 				mutate(size = 
-								case_when(
-									type == 'SNP' ~ '0',
-									type == 'SUB' ~ a,
-									type == 'DEL' ~ a
-								)
+					case_when(
+						type == 'SNP' ~ '0',
+						type == 'SUB' ~ a,
+						type == 'DEL' ~ a
+					)
 				) %>%
 				select(-a, -b)
-		
 	}
